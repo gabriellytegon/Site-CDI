e01b81088161b2467dd38162460b534942909e32
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-06-06 14:06:08 -0300

    feat: adicionando a pagina vida no campus no routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import Eventos from "./pages/Eventos";
+import VidaNoCampus from "./pages/vidanocampus";
 
 export const router = createBrowserRouter([
   {
@@ -11,4 +12,8 @@ export const router = createBrowserRouter([
     path: "/eventos",
     element: <Eventos/>,
   },
+  {
+    path: '/vidanocampus',
+    element: <VidaNoCampus/>
+  }
 ]);
