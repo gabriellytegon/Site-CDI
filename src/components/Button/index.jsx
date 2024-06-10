2e24410aca7868a60a328e971107d6dd223c8aa3
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-06-10 19:06:49 -0300

    style: alterando o modo de width do componente Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,18 +1,19 @@
 import './style.css'
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
-const Button = ({buttonText, link, option, width, fontSize}) => {
+const Button = ({buttonText, link, option, fontSize}) => {
     return (
-      <a 
-        href={link} 
+      <Link
         target="_blank" 
         rel='noreferrer'
         id='button'
+        to={link}
         className={option == 0 ? 'button1' : option == 1 ? 'button2' : ''}
-        style={{width: width, fontSize: fontSize}}
+        style={{fontSize: fontSize}}
       >
         {buttonText}
-      </a>
+      </Link>
     );
   };
   
