839be1d98d51430c0407229c4cc5fbcf2fbc98da
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-06-28 18:49:42 -0300

    feat: adicionando cardapio na pagina vida no campus

---
+++ src/components/Cardapio/index.jsx
@@ -0,0 +1,8 @@
+import './style.css'
+
+const Cardapio  = () => {
+
+    return <iframe id="iframeCardapio" src="https://www.sar.unicamp.br/RU/view/site/cardapio.php"></iframe>
+}
+
+export default Cardapio;
