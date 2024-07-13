bcdf045d2d23c02996ea4bb44c76487af6c21586
higorgb <higor.gabriel930@gmail.com>
2024-07-12 21:18:31 -0300

    Adicionando Footer na Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const Home = () => {
     return(
         <>
             <Header></Header>
             <h2>Home</h2>
+            <Footer></Footer>
+
         </>
     )
 }
