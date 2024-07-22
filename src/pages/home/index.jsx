aabd4633740bf8514f67481cb442a76f5bda01d0
Thyago <t247602@dac.unicamp.br>
2024-07-22 02:20:14 -0300

    style: alteração da home com alterações do figma

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -17,19 +17,20 @@ const Home = () => {
                 <h1>O que é a CDI?</h1>
                 <p>
                     Além de ser um canal oficial de comunicação entre os alunos e os docentes, organizamos uma série de eventos como: cursos, palestras e visitas técnicas em parceria com a comunidade e com empresas tech.
+                    <br></br>
                     Nós trabalhamos para que sua jornada na Universidade seja aproveitada da melhor forma possível! Conte com a CDI durante seu ano letivo para tirar dúvidas e sugerir ideias.
                 </p>
                 <div id="conceitos">
                     <div className="conceito">
                         <h2>Missão</h2> <br />
                         <p>
-                            Nós da CDI, apoiamos a comunidade de T.I. da FT e desenvolvemos os membros da comissão para que suas jornadas acadêmicas sejam as mais enriquecedoras
+                            Apoiamos a comunidade de T.I. da FT e desenvolvemos os membros da comissão para que suas jornadas acadêmicas sejam as mais enriquecedoras.
                         </p>
                     </div>
                     <div className="conceito">
                         <h2>Visão</h2> <br />
                         <p>
-                            Trata-se do nosso objetivo maior, até 2026, pretendemos aumentar em 50% nosso impacto na comunidade de T.I.
+                            Até 2026, pretendemos aumentar em 50% nosso impacto na comunidade de T.I.
                         </p>
                     </div>
                     <div className="conceito">
@@ -52,17 +53,17 @@ const Home = () => {
                     <div className="curso_texto">
                         <h2>Bacharelado em Sistemas de Informação</h2>
                         <p>O curso de S.I possui um papel fundamental em um mundo que viabiliza a rapidez da informação pois a compreensão dos sistemas informacionais é de extrema importância. O profissional qualificado pela área desenvolve duas principais habilidades: projetar e implantar os sistemas que integram o mundo digital.</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Período: Integral</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Carga horária estimada: 3.060 horas</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Integralização: 08 semestres</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Vagas: 45</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Período: Integral</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Carga horária estimada: 3.060 horas</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Integralização: 08 semestres</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Vagas: 45</p>
                         <div className="botao">
                             <Button
                                 buttonText={"Acesse o currículo pleno"}
                                 width={200}
                                 fontSize={14}
                                 link={'https://www.dac.unicamp.br/sistemas/catalogos/grad/catalogo2024/cursos/94g/curriculo.html'}
-                                option={0}
+                                option={1}
                             />
                         </div>
                     </div>
@@ -72,17 +73,17 @@ const Home = () => {
                     <div className="curso_texto">
                         <h2>Tecnologia em Análise e Desenvolvimento de Sistemas</h2>
                         <p>O curso de T.A.D.S, têm como intuito formar profissionais com uma base sólida, de modo ao qual seja prático aplicar os conhecimentos teóricos na intensa vivência prática que o mundo da tecnologia proporciona.</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Período: Noturno</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Carga horária estimada: 2.430 horas</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Integralização: 07 semestres</p>
-                        <p><img src={checkIcon} alt="icone de check" /> Vagas: 45</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Período: Noturno</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Carga horária estimada: 2.430 horas</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Integralização: 07 semestres</p>
+                        <p className="item"><img src={checkIcon} alt="icone de check" /> Vagas: 45</p>
                         <div className="botao">
                             <Button
                                 buttonText={"Acesse o currículo pleno"}
                                 width={200}
                                 fontSize={14}
                                 link={'https://www.dac.unicamp.br/sistemas/catalogos/grad/catalogo2024/cursos/36t/curriculo.html'}
-                                option={0}
+                                option={1}
                             />
                         </div>
                     </div>
@@ -91,7 +92,7 @@ const Home = () => {
             </div>
 
             <div id="equipe">
-                <h1>Quem faz parte da CDI?</h1>
+                <h1>Conheça nossos membros!</h1>
 
                 <div className="area">
                     <h2>Presidência</h2>
