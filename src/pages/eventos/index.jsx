f15ed52096353906140186a3e42d48ff3b79db21
hitalloazevedo <azevedohitallo@gmail.com>
2024-09-17 17:29:28 -0300

    fix: alinhando a página e deixando responsiva, ajustando container de cards

--- src/pages/eventos/index.jsx
+++ src/pages/eventos/index.jsx
@@ -19,35 +19,29 @@ const Eventos = () => {
         <>
             <Header title="Eventos" content="Fique por dentro dos principais eventos da FT!" />
 
-            <div className="container-fluid mb-5 mt-5">
-                <div className="row justify-content-center">
-                    <div className="pt-3 w-100">
-                        <h2 className="tittle text-center mb-3 titles-h2">Eventos já realizados</h2>
-                        <p>A CDI é campeã em proporcionar experiências ÚNICAS e TRANSFORMADORAS. Acreditamos no poder da vivência a cima de tudo e por isso sempre nos preocupamos em preparar atividades imersivas aos alunos.</p>
-                        <div id="carousel-eventos">
-                            <CardCarousel photos={photos}></CardCarousel>
-                        </div>
-                    </div>
-
+            <div className="container-eventos">
+                <h2 className="tittle text-center mb-3 titles-h2">Eventos já realizados</h2>
+                <p>A CDI é campeã em proporcionar experiências ÚNICAS e TRANSFORMADORAS. Acreditamos no poder da vivência a cima de tudo e por isso sempre nos preocupamos em preparar atividades imersivas aos alunos.</p>
+                <div id="carousel-eventos">
+                    <CardCarousel photos={photos}></CardCarousel>
                 </div>
             </div>
-
 
 
             <div className="container-fluid mb-5">
                 <div className="row justify-content-center">
                     <div className="col-9">
-                        <div className="d-flex justify-content-end">
+                        <div className="pinguim-container d-flex justify-content-end">
                             <img src={pinguim} width={"200"}></img>
                         </div>
                     </div>
 
                 </div>
                 <div className="row justify-content-center">
-                    <div className="col-10 pt-3 eventContent">
+                    <div className="col-10 pt-5 pb-5 eventContent">
                         <h2 className="tittle text-center titles-h2">Mural de Eventos</h2>
                         <p className="text-center">Acompanhe o nosso mural para ficar por dentro dos próximos eventos que ocorrerão:</p>
-                        <div className="aling-card">
+                        <div className="aling-card flex gap-3">
                             <CardEvento
                                 width="820px"
                                 height="220px"
