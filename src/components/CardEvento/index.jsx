1cc4a7b7b2cdd699e00891bf04e6f856940800f2
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-10-18 09:47:54 -0300

    refactor: reestruturando o card evento

--- src/components/CardEvento/index.jsx
+++ src/components/CardEvento/index.jsx
@@ -2,41 +2,28 @@ import './style.css';
 
 function CardEvento(props) {
     return (
-        <section>
-            <div className='card-evento'>
-                <div className='info-evento'>
+        <div className='card-evento'>
+            <div className='info-evento'>
+                <div className="event-details">
+                    <h3>{props.nomeEvento}</h3>
+                    <p>Descrição: {props.descricao}</p>
+                    <p>Data: {props.data}</p>
+                    <p>Local: {props.local}</p>
+                    <p>Hora: {props.hora}</p>
+                </div>
 
-                    <div className='title'>Nome do Evento <div className='subTitle'>Descrição: <span>{props.descricao}</span></div></div>
-
-                    <div className="textBody">
-                        <div className='subTitle'>Data: <span>{props.data}</span></div>
-                        <div className='subTitle'>Hora: <span>{props.hora}</span></div>
-                        <div className='subTitle mb-3'>Local: <span>{props.link}</span></div>
-                        <button type="button" className="btn btn-primary">Inscreva-se aqui</button>
-                    </div>
+                <div>
+                    <a type="button" className="btn btn-primary">Inscreva-se aqui</a>
 
                 </div>
-                <div className='imagem-evento'>
-                    <div className="circleImg">
-                        <img src={props.imageUrl}></img>
-                    </div>
-                </div>
+
             </div>
-
-
-            {/* <div style={title}>{props.title}</div>
-            <div style={cardStyle}>
-
-                <div className="textBody">
-                    <div>Data: {props.data}</div>
-                    <div>Hora: {props.hora}</div>
-                    <div>Link para inscrição: {props.link}</div>
-                </div>
+            <div className='imagem-evento'>
                 <div className="circleImg">
                     <img src={props.imageUrl}></img>
                 </div>
-            </div> */}
-        </section>
+            </div>
+        </div>
     );
 }
 
